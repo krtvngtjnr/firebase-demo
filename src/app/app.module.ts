e3f6aecac71e0f6c4c775643f7bfa90bbb5bed5b
krtvngtjnr <krtvngtjnr@gmail.com>
2020-11-22 16:23:53 +0900

    Pipeを使って日付を変換

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { ChatDatePipe } from './pipe/chat-date.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatDatePipe
   ],
   imports: [
     BrowserModule,
--- src/app/class/comment.ts
+++ src/app/class/comment.ts
@@ -1,3 +1,4 @@
+import * as moment from 'moment';
 import { User } from './user';
 
 export class Comment {
@@ -5,10 +6,12 @@ export class Comment {
   user: User;
   initial: string;
   content: string;
+  date: number;
 
   constructor(user: User, content: string) {
     this.user = user;
     this.initial = user.name.slice(0, 1);
     this.content = content;
+    this.date = +moment();
   }
 }
